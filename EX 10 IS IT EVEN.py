e2f90893af2ee310480f4e482b27d288a710189d
liannekristinedocena <docena.lianne.kristine@gmail.com>
2025-10-24 00:20:20 +0400

    Update EX 10 IS IT EVEN.py

--- EX 10 IS IT EVEN.py
+++ EX 10 IS IT EVEN.py
@@ -7,12 +7,13 @@ def even_odd(number): #defines the function even_odd that will determine whether
         return f"{number} is odd." #if not divisible, the code will return a message saying that the number is odd
         
 def main(): #defines the main function 
-    number_2 = int(input("Please enter a number: ")) #asks the user to input a number that will be placed in variable_2
+    number_2 = int(input("Please enter a number: ")) #asks the user to input a number that will be placed in variable number_2
     
     result = even_odd(number_2) #calls the even_odd function with user input in a variable called result 
     print(result) #prints the result 
     
 if __name__ == "__main__": #enables the program to run properly 
     main() #calls onto the main function to execute the program 
+
 
 
