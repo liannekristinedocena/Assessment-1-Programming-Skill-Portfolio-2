788d498e833001a645176626a2a6adb7c36b8425
liannekristinedocena <docena.lianne.kristine@gmail.com>
2025-10-25 23:21:29 +0400

    Update EX4 PRIMITIVE QUIZ.py

--- EX4 PRIMITIVE QUIZ.py
+++ EX4 PRIMITIVE QUIZ.py
@@ -2,8 +2,9 @@
 
 answer = input("What is the capital of france? : ") #asks for user input and places it in the variable 'answer' 
 
-if answer == "Paris" : #uses if condition. IF user input in the variable answer is equals to "Paris"
+if answer == "Paris" : #uses if condition. IF user input in the variable answer is equals to "Paris" then,
     print("Your answer is correct! Congratulations.") #Prints out congratulations message
 else: 
     print("Your answer is wrong!") #If user input not equal to "Paris" then the answer is wrong. 
+
 
